16c481b400351eead8362bb998acdef9b5fd5d88
Sam Kilpatrick <samkilpatrick903@gmail.com>
2021-10-27 08:40:24 -0500

    starting js application

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const Manager= require('./employees/manager');
+const Engineer = require('./employees/engineer');
+const Intern = require('./employees/intern');
+
